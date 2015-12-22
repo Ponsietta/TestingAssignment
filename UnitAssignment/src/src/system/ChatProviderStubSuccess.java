6c65a6deec841218b64f93f75d8133c354abaea4
Ponsietta <marziacutajar@gmail.com>
2015-12-22 12:52:37 +0100

    aloha

--- UnitAssignment/src/src/system/ChatProviderStubSuccess.java
+++ UnitAssignment/src/src/system/ChatProviderStubSuccess.java
@@ -3,29 +3,31 @@ package src.system;
 public class ChatProviderStubSuccess implements ChatProvider {
 
 	@Override
-	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+	public String getName() 
+	{
+		return this.getClass().getName();
 	}
 
 	@Override
-	public int sendMessageTo(String friendID, String msg) {
+	public int sendMessageTo(String friendID, String msg) 
+	{
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public void onMessageReceived(String text) {
+	public void onMessageReceived(String text) 
+	{
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public int connect(String username, String password) {
+	public int connect(String username, String password) 
+	{
 		if (username == null || username.trim() == "" || password == null || password.trim() == "" 
 		|| !username.equals("rebmar") || !password.equals("enternow"))
 		{
-
 			return 1;
 		}
 
@@ -33,8 +35,8 @@ public class ChatProviderStubSuccess implements ChatProvider {
 	}
 
 	@Override
-	public int getMaxMessageLength() {
-		// TODO Auto-generated method stub
+	public int getMaxMessageLength() 
+	{
 		return 50;
 	}
 
