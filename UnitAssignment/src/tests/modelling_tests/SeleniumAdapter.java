b3736320f5d3e5e4262b776a2b829c64c6ba6d54
Ponsietta <marziacutajar@gmail.com>
2016-01-07 19:43:15 +0100

    additions to adapter

--- UnitAssignment/src/tests/modelling_tests/SeleniumAdapter.java
+++ UnitAssignment/src/tests/modelling_tests/SeleniumAdapter.java
@@ -1,7 +1,5 @@
 package tests.modelling_tests;
 import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import tests.web_tests.StepDefinitions.*;
 
 public class SeleniumAdapter {
@@ -20,6 +18,38 @@ public class SeleniumAdapter {
 	{
 	      commonSteps.TearDown();
 	      commonSteps.StartUp();
+	}
+	
+	public void loginValid()
+	{
+		loginSteps.GivenIAmAUserTryingToLogIn();
+		loginSteps.WhenILoginUsingValidCredentials();
+		chatSteps.ThenIShouldBeTakenToTheChatPage();
+	}
+	
+	public void loginInvalid()
+	{
+		loginSteps.GivenIAmAUserTryingToLogIn();
+		loginSteps.WhenILoginUsingInvalidCredentials();
+		loginSteps.ThenIShouldSeeAnErrorMessage();
+		loginSteps.ThenIShouldRemainOnTheLoginPage();
+	}
+	
+	public void SendMessageTriggerParentalLock()
+	{
+		loginSteps.GivenIAmALoggedInUser();
+		chatSteps.GivenIAmOnTheChatPage();
+		chatSteps.WhenISendAMessageWith("Fudge");
+		chatSteps.ThenAnErrorWillTellMeThatTheMessageWasNotSent();
+		chatSteps.ThenTheMessageShouldNotAppearInMyTextWindow();
+	}
+	
+	public void logout()
+	{
+		loginSteps.GivenIAmALoggedInUser();
+		chatSteps.GivenIAmOnTheChatPage();
+		loginSteps.WhenISendClickOnLogOut();
+		loginSteps.ThenIShouldBeLoggedOut();
 	}
 
 	
